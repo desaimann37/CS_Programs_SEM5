0d63c8c9fa21de688fce58d63a89aeb0238ed2d9
MANN DESAI <desaimann37@gmail.com>
2023-07-16 00:03:16 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+    Author : Mann Desai
+    Objective : Check for Palindromic Number 
+*/
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public bool IsPalindrome(String num)
+        {
+            
+            int i = 0;
+            int j = num.Length - 1;
+            while (i <= j)
+            {
+                if (num[i] != num[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Something to Check that is it palindrome? : ");
+            String num = Console.ReadLine();
+            Program p = new Program();
+            bool flag = p.IsPalindrome(num);
+            if (flag == true)
+            {
+                Console.WriteLine("{0} is Palindrome" , num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not Palindrome", num);
+            }
+       
+        }
+    }
+}
